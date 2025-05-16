cdd0518e3b23ba48b907b14af33282f43b550359
Bilgekaan36 <b.yilmaz@workdigital.de>
2025-05-16 19:01:46 +0200

    implement round based oencil removing per userinput

--- src/lastpencil/Main.java
+++ src/lastpencil/Main.java
@@ -1,23 +1,36 @@
 package lastpencil;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] users = new String[] {"John", "Jack"};
+        String[] players = {"John", "Jack"};
 
         System.out.println("How many pencils would you like to use:");
         int pencils = scanner.nextInt();
+        scanner.nextLine(); // consume newline
 
         System.out.println("Who will be the first (John, Jack):");
-        String inputName = scanner.next();
+        String currentPlayer = scanner.nextLine();
 
-        for (int i = 0; i < pencils; i++) {
-            System.out.print("|");
+        while (pencils > 0) {
+            // Print pencils
+            for (int i = 0; i < pencils; i++) {
+                System.out.print("|");
+            }
+            System.out.println();
+
+            // Print turn
+            System.out.println(currentPlayer + "'s turn:");
+
+            // Get input
+            int taken = scanner.nextInt();
+
+            pencils -= taken;
+
+            // Switch player
+            currentPlayer = currentPlayer.equals(players[0]) ? players[1] : players[0];
         }
-        System.out.println();
-        System.out.printf("%s is going first!", Arrays.stream(users).filter(user -> user.equals(inputName)).findFirst().orElse("User not found!"));
     }
 }
